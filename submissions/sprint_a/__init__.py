d5af3a8332d89009fca4c13e9f8c9432dfc4285f
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2019-02-06 13:55:11 -0500

    sprinta

---
+++ submissions/sprint_a/__init__.py
@@ -0,0 +1,11 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+
+
+def configure_app():
+    """configures the app and sets the env variables."""
+    app = Flask(__name__)
+    PROJECT_ROOT = os.path.dirname(__file__)
+    load_dotenv(os.path.join(PROJECT_ROOT, '.env'))
+    return app
