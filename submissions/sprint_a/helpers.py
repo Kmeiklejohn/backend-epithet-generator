fac458fc3177f854408483233e5524044da000a5
Kyle Meiklejohn <kmeiklejohn4@gmail.com>
2019-02-07 11:33:37 -0500

    b setup

---
+++ submissions/sprint_a/helpers.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+class FileManager:
+    """Handle local file system IO."""
+
+    @staticmethod
+    def get_extension(path):
+        """Get file extension from file path."""
+        return os.path.splitext(path)[-1][1:]
+
+    @staticmethod
+    def read_json(path, mode='r', *args, **kwargs):
+        with open(path, mode=mode, *args, **kwargs) as handle:
+            return json.load(handle)
